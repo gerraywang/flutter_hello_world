0005581978fe1e61615de259bd328b2d5b991619
gerraywang <wang_yuquan@retail-ai.jp>
2024-01-06 21:41:47 +0900

    delete env

--- lib/env.dart
+++
@@ -1,9 +0,0 @@
-import 'package:envied/envied.dart';
-
-part 'env.g.dart';
-
-@Envied(path: '.env.dev')
-final class Env {
-    @EnviedField(varName: 'ENV')
-    static const String env = _Env.env;
-}
